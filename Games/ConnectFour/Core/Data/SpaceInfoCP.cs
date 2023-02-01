0eac3fbcca1071688af0ef8ef5f4f52a82d01b17
Andy Walter <musictopia2@hotmail.com>
2023-02-01 08:35:36 -0700

    updated connect four to .net 7.

--- Games/ConnectFour/Core/Data/SpaceInfoCP.cs
+++ Games/ConnectFour/Core/Data/SpaceInfoCP.cs
@@ -3,12 +3,12 @@ public class SpaceInfoCP : IBasicSpace
 {
     public Vector Vector { get; set; }
     public int Player { get; set; }
-    public string Color { get; set; } = cs.Transparent;
+    public string Color { get; set; } = cs1.Transparent;
     public bool HasImage => Player > 0;
     public void ClearSpace()
     {
         Player = 0;
-        Color = cs.Transparent;
+        Color = cs1.Transparent;
     }
     public bool IsFilled()
     {
