eedb5ad34b047ab826b6b5ae588404a343d1f2b5
Andy Walter <musictopia2@hotmail.com>
2023-06-26 09:04:22 -0700

    Was forced to not allow new game for clue board game. Because there are too many stuff from old game that is held on to.

--- Games/ClueBoardGame/Core/ViewModels/ClueBoardGameShellViewModel.cs
+++ Games/ClueBoardGame/Core/ViewModels/ClueBoardGameShellViewModel.cs
@@ -1,6 +1,7 @@
 namespace ClueBoardGame.Core.ViewModels;
 public class ClueBoardGameShellViewModel : BasicBoardGamesShellViewModel<ClueBoardGamePlayerItem>
 {
+    private readonly IMessageBox _message;
     public ClueBoardGameShellViewModel(IGamePackageResolver mainContainer,
         CommandContainer container,
         IGameInfo gameData,
@@ -8,14 +9,20 @@ public class ClueBoardGameShellViewModel : BasicBoardGamesShellViewModel<ClueBoa
         IMultiplayerSaveState save,
         TestOptions test,
         IEventAggregator aggregator,
-        IToast toast
+        IToast toast,
+        IMessageBox message
         )
         : base(mainContainer, container, gameData, basicData, save, test, aggregator, toast)
     {
+        _message = message;
     }
     protected override IMainScreen GetMainViewModel()
     {
         var model = MainContainer.Resolve<ClueBoardGameMainViewModel>();
         return model;
+    }
+    protected override async Task ShowNewGameAsync()
+    {
+        await _message.ShowMessageAsync("Game is over.  However, unable to allow for new game.  If you want new game, close out and reconnect again.");
     }
 }
