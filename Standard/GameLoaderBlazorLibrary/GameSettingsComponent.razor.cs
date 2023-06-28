b64b1354861ed66e150e7fd27b05d0f2428b1d85
Andy Walter <musictopia2@hotmail.com>
2023-06-28 09:49:16 -0700

    Added options to start allowing player options.

--- Standard/GameLoaderBlazorLibrary/GameSettingsComponent.razor.cs
+++ Standard/GameLoaderBlazorLibrary/GameSettingsComponent.razor.cs
@@ -7,7 +7,7 @@ public partial class GameSettingsComponent
     IJSRuntime? JS { get; set; } //this should have its own js.
     [Inject]
     private IToast? Toast { get; set; }
-    private static string GetRows => gg1.RepeatAuto(7); //for now, 6 instead of 7 (?)
+    private static string GetRows => gg1.RepeatAuto(11); //for now, 6 instead of 7 (?)
     private static string GetColumns => gg1.RepeatMaximum(2); //may eventually do a special grid control that specialize in adding in data and it would just work.
     private bool _beginaccept;
     protected override void OnInitialized()
@@ -51,6 +51,10 @@ public partial class GameSettingsComponent
         {
             BasicGameFrameworkLibrary.Core.MiscProcesses.GlobalVariables.DoUseHome = false;
         }
+    }
+    private static void UpdatePlayerOptions(EnumPlayerMode mode)
+    {
+        GlobalDataModel.DataContext!.PlayerMode = mode;
     }
     private async Task SaveChangesAsync()
     {
