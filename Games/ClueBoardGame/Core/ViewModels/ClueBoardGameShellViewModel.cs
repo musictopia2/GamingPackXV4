aa1f2fd92654b0245adefd1a13790a05baf1ab92
Andy Walter <musictopia2@hotmail.com>
2024-02-19 18:19:28 -0700

    Taking out code to not allow new game. Since clue and payday now allows new game.

--- Games/ClueBoardGame/Core/ViewModels/ClueBoardGameShellViewModel.cs
+++ Games/ClueBoardGame/Core/ViewModels/ClueBoardGameShellViewModel.cs
@@ -1,30 +1,17 @@
 namespace ClueBoardGame.Core.ViewModels;
-public class ClueBoardGameShellViewModel : BasicBoardGamesShellViewModel<ClueBoardGamePlayerItem>
+public class ClueBoardGameShellViewModel(IGamePackageResolver mainContainer,
+    CommandContainer container,
+    IGameInfo gameData,
+    BasicData basicData,
+    IMultiplayerSaveState save,
+    TestOptions test,
+    IEventAggregator aggregator,
+    IToast toast
+        ) : BasicBoardGamesShellViewModel<ClueBoardGamePlayerItem>(mainContainer, container, gameData, basicData, save, test, aggregator, toast)
 {
-    private readonly IMessageBox _message;
-    public ClueBoardGameShellViewModel(IGamePackageResolver mainContainer,
-        CommandContainer container,
-        IGameInfo gameData,
-        BasicData basicData,
-        IMultiplayerSaveState save,
-        TestOptions test,
-        IEventAggregator aggregator,
-        IToast toast,
-        IMessageBox message
-        )
-        : base(mainContainer, container, gameData, basicData, save, test, aggregator, toast)
-    {
-        _message = message;
-    }
     protected override IMainScreen GetMainViewModel()
     {
         var model = MainContainer.Resolve<ClueBoardGameMainViewModel>();
         return model;
     }
-    //try to allow new game for clue board game now.
-
-    //protected override async Task ShowNewGameAsync()
-    //{
-    //    await _message.ShowMessageAsync("Game is over.  However, unable to allow for new game.  If you want new game, close out and reconnect again.");
-    //}
 }
--- Games/Payday/Core/ViewModels/PaydayShellViewModel.cs
+++ Games/Payday/Core/ViewModels/PaydayShellViewModel.cs
@@ -1,32 +1,17 @@
 namespace Payday.Core.ViewModels;
-public class PaydayShellViewModel : BasicBoardGamesShellViewModel<PaydayPlayerItem>
+public class PaydayShellViewModel(IGamePackageResolver mainContainer,
+    CommandContainer container,
+    IGameInfo gameData,
+    BasicData basicData,
+    IMultiplayerSaveState save,
+    TestOptions test,
+    IEventAggregator aggregator,
+    IToast toast
+        ) : BasicBoardGamesShellViewModel<PaydayPlayerItem>(mainContainer, container, gameData, basicData, save, test, aggregator, toast)
 {
-    //private readonly IMessageBox _message;
-    public PaydayShellViewModel(IGamePackageResolver mainContainer,
-        CommandContainer container,
-        IGameInfo gameData,
-        BasicData basicData,
-        IMultiplayerSaveState save,
-        TestOptions test,
-        IEventAggregator aggregator,
-        IToast toast,
-        IMessageBox message
-        )
-        : base(mainContainer, container, gameData, basicData, save, test, aggregator, toast)
-    {
-        //_message = message;
-    }
-    //protected override void ReplaceVMData()
-    //{
-    //    //ignore but reserves the right to do something else if needed.
-    //}
     protected override IMainScreen GetMainViewModel()
     {
         var model = MainContainer.Resolve<PaydayMainViewModel>();
         return model;
     }
-    //protected override async Task ShowNewGameAsync()
-    //{
-    //    await _message.ShowMessageAsync("Game is over.  However, unable to allow for new game.  If you want new game, close out and reconnect again.");
-    //}
 }
