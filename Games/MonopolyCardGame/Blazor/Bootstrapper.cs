6608e37321de53d45cb83db7af081da5c766d157
Andy Walter <musictopia2@hotmail.com>
2024-02-21 08:17:02 -0700

    Created test to show problem on monopoly card game that shows the need to manually figure out monopoly even when trying to see if you can go out.

--- Games/MonopolyCardGame/Blazor/Bootstrapper.cs
+++ Games/MonopolyCardGame/Blazor/Bootstrapper.cs
@@ -4,18 +4,18 @@ public class Bootstrapper : MultiplayerBasicBootstrapper<MonopolyCardGameShellVi
     public Bootstrapper(IStartUp starts, EnumGamePackageMode mode) : base(starts, mode)
     {
     }
-    //protected override Task RegisterTestsAsync()
-    //{
-    //    TestData!.SaveOption = EnumTestSaveCategory.RestoreOnly; //so i can test going out with wilds everytime.
-    //    var container = GetDIContainer;
-    //    container.RegisterSingleton<ITestCardSetUp<MonopolyCardGameCardInformation, MonopolyCardGamePlayerItem>, TestCards>();
-    //    return base.RegisterTestsAsync();
-    //}
     protected override Task RegisterTestsAsync()
     {
-        TestData!.SaveOption = EnumTestSaveCategory.RestoreOnly;
+        TestData!.SaveOption = EnumTestSaveCategory.RestoreOnly; //so i can test going out with wilds everytime.
+        var container = GetDIContainer;
+        container.RegisterSingleton<ITestCardSetUp<MonopolyCardGameCardInformation, MonopolyCardGamePlayerItem>, TestCards>();
         return base.RegisterTestsAsync();
     }
+    //protected override Task RegisterTestsAsync()
+    //{
+    //    TestData!.SaveOption = EnumTestSaveCategory.RestoreOnly;
+    //    return base.RegisterTestsAsync();
+    //}
     protected override Task ConfigureAsync(IGamePackageRegister register)
     {
         Core.DIFinishProcesses.GlobalDIAutoRegisterClass.RegisterNonSavedClasses(GetDIContainer);
--- Games/MonopolyCardGame/Blazor/TestCards.cs
+++ Games/MonopolyCardGame/Blazor/TestCards.cs
@@ -3,16 +3,36 @@ public class TestCards : ITestCardSetUp<MonopolyCardGameCardInformation, Monopol
 {
     Task ITestCardSetUp<MonopolyCardGameCardInformation, MonopolyCardGamePlayerItem>.SetUpTestHandsAsync(PlayerCollection<MonopolyCardGamePlayerItem> playerList, IListShuffler<MonopolyCardGameCardInformation> deckList)
     {
-        var first = playerList.First();
+        //var first = playerList.First();
 
-        var second = playerList.Last();
-        var cards = deckList.Where(x => x.WhatCard == EnumCardType.IsMr).Take(2);
-        first.StartUpList.AddRange(cards);
-        cards = deckList.Where(x => x.WhatCard == EnumCardType.IsMr).Skip(2).Take(1);
-        second.StartUpList.AddRange(cards);
-        //var player = playerList.GetSelf();
-        //var cards = deckList.Where(x => x.WhatCard == EnumCardType.IsChance).Take(1);
-        //player.StartUpList.AddRange(cards);
+        //var second = playerList.Last();
+        //var cards = deckList.Where(x => x.WhatCard == EnumCardType.IsMr).Take(2);
+        //first.StartUpList.AddRange(cards);
+        //cards = deckList.Where(x => x.WhatCard == EnumCardType.IsMr).Skip(2).Take(1);
+        //second.StartUpList.AddRange(cards);
+        var player = playerList.GetSelf();
+
+
+
+        var cards = deckList.Where(x => x.WhatCard == EnumCardType.IsChance).Take(2);
+        player.StartUpList.AddRange(cards);
+
+        cards = deckList.Where(x => x.WhatCard == EnumCardType.IsHotel).Take(1);
+        player.StartUpList.AddRange(cards);
+        4.Times(y =>
+        {
+            if (y != 3)
+            {
+                cards = deckList.Where(x => x.WhatCard == EnumCardType.IsHouse && x.HouseNum == y).Take(1);
+                player.StartUpList.AddRange(cards);
+            }
+        });
+        cards = deckList.Where(x => x.WhatCard == EnumCardType.IsToken).Take(1);
+        player.StartUpList.AddRange(cards);
+        cards = deckList.Where(x => x.WhatCard == EnumCardType.IsRailRoad).Take(2);
+        player.StartUpList.AddRange(cards);
+        cards = deckList.Where(x => x.Money == 400 && x.WhatCard == EnumCardType.IsProperty).Take(1);
+        player.StartUpList.AddRange(cards);
         return Task.CompletedTask;
     }
 }
