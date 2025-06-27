dfbb54587c21ae17835183a2d76d7ab192f48ae6
Andy Walter <musictopia2@hotmail.com>
2025-06-27 14:21:58 -0700

    Refactor CSS management and update package version
    - Updated `GameCssStartup.razor` to use `CssSimpleSetup` for CSS file management and removed previous implementation.
    - Modified `GameCssStartup.razor.cs` to change the namespace, remove unnecessary parameters, and define `_files` for CSS files.
    - Enhanced `App.razor` with additional using directives and added a check for `AppStyleName`.
    - Updated `App.razor.cs` to include a static `AppStyleName` property and retain the toast timeout setting.
    - Changed `BasicGameFrameworkLibrary` package version from `9.0.23` to `9.0.25` in the project file.

--- Standard/BasicGameFrameworkLibrary/Blazor/StartupClasses/App.razor.cs
+++ Standard/BasicGameFrameworkLibrary/Blazor/StartupClasses/App.razor.cs
@@ -1,14 +1,10 @@
 namespace BasicGameFrameworkLibrary.Blazor.StartupClasses;
-public partial class GameCssStartup
+public partial class App
 {
+    public static string AppStyleName { get; set; } = "";
     private BasicList<string> _files = ["_content/BasicGameFrameworkLibrary/basicgaming.css"];
-    [Parameter]
-    public string AppStyleName { get; set; } = "";
-    [Parameter]
-    public RenderFragment? ChildContent { get; set; }
     protected override void OnInitialized()
     {
         BasicBlazorLibrary.Components.Toasts.BlazoredToasts.Timeout = 3;
-        base.OnInitialized();
     }
 }
--- Standard/GameLoaderBlazorLibrary/App.razor.cs
+++
@@ -1,6 +0,0 @@
-namespace GameLoaderBlazorLibrary;
-public partial class App
-{
-    [Parameter]
-    public string AppStyleName { get; set; } = "";
-}
